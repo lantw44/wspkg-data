caa5649fd1a1a35ad6a6264c7b4abea372ca6d61
Ting-Wei Lan <lantw44@gmail.com>
2014-04-30 08:09:02 +0800

    pkgbuild: 補上 diglog4ports，執行 poudriere options 時會用到

--- pkgbuild.h
+++ pkgbuild.h
@@ -1,7 +1,10 @@
 // Basic packages
  bash bash-completion bind-tools cmdwatch git ldapvi
- lighttpd ncdu portlint poudriere-devel pstree py27-ranger screen
+ lighttpd ncdu portlint pstree py27-ranger screen
  subversion sudo tig tree vim-lite w3m wget
+
+// Poudriere
+ poudriere-devel dialog4ports
 
 // CGit and gitolite
  cgit gitolite highlight
