5183888f7c68fff32e5a0b698825d9016a7c0c7d
Ting-Wei Lan <lantw44@gmail.com>
2014-07-17 16:30:20 +0800

    mrtg: 加入 217-mrtg.sh 用作 post-update hook

---
+++ 217-mrtg.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+write_string_to_fd () {
+	fd="$1"
+	shift
+	echo "$@" 1>&"$fd"
+}
+
+write_stdin_to_fd () {
+	fd="$1"
+	shift
+	cat 1>&"$fd"
+}
+
+echo_cmd () {
+	printf "\033[1;33mRunning command => \033[m"
+	echo "$@"
+	"$@"
+	echo
+}
+
+export WRKDIR="/tmp/wspkg-build"
+export WSPKGDIR="${WRKDIR}/wspkg"
+export REPODIR="/usr/local/git/repositories"
+export PORTSDIR="/usr/local/poudriere/ports/default"
+
+
+# Update sources
+for repo in wspkg wspkg-data; do
+	if [ '!' -d "${WRKDIR}/${repo}" ]; then
+		echo_cmd git clone "${REPODIR}/${repo}.git" "${WRKDIR}/${repo}"
+	else
+		echo_cmd cd "${WRKDIR}/${repo}"
+		echo_cmd git pull "${REPODIR}/${repo}.git"
+	fi
+done
+
+
+# Fetch new index file
+cd "${PORTSDIR}"
+[ -z "${DEBUG}" ] && echo_cmd make fetchindex
+
+
+# Setup wspkg
+cd "${WRKDIR}/wspkg"
+echo_cmd make ASCIIDOC=true PANDOC=true
+
+
+# Generate lists from wslinux
+cd "${WRKDIR}/wspkg-data"
+echo_cmd ./build.sh wslinux wslinux/out/wslinux.list
+
+exec 3< "wslinux/out/wslinux.list"
+exec 4> "${WRKDIR}/wslinux.html"
+
+while read pkg 0<&3; do
+	write_string_to_fd 4 "<li><a href='https://packages.debian.org/testing/${pkg}'>${pkg}</a></li>"
+done
+
+exec 3<&-
+exec 4>&-
+
+
+# Generate pages for wsbsd
+cd "${WRKDIR}/wspkg-data"
+echo_cmd ./build.sh wsbsd wsbsd/out/wsbsd.ports
+
+exec 3< "wsbsd/out/wsbsd.ports"
+exec 4> "${WRKDIR}/wsbsd.html"
+
+while read pkg origin 0<&3; do
+	write_string_to_fd 4 "<li><a href='https://www.freshports.org/${origin}'>${pkg}</a></li>"
+done
+
+exec 3<&-
+exec 4>&-
+
+
+# Copy to mrtg.csie.ntu.edu.tw
+echo_cmd scp -i /usr/local/git/ssh-keys/to-mrtg \
+	"${WRKDIR}/wslinux.html" \
+	pkgbuild@mrtg.csie.ntu.edu.tw:/var/www/wslinux.html
+
+echo_cmd scp -i /usr/local/git/ssh-keys/to-mrtg \
+	"${WRKDIR}/wsbsd.html" \
+	pkgbuild@mrtg.csie.ntu.edu.tw:/var/www/wsbsd.html
