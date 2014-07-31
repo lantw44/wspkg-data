9877c83c07557f8387f10e32437f4be3f614d480
Ting-Wei Lan <lantw44@gmail.com>
2014-07-31 20:14:52 +0800

    改用新的方式填寫設定值，移除所有不再需要的變數

--- wsbsd.mk
+++ wsbsd.mk
@@ -1,36 +1,11 @@
 # vim: set ts=8 sts=8 sw=8 ft=make:
 # CSIE workstation bsd meta-package
 
-# My name and my directories
 NAME=                   wsbsd
-INDIR=                  $(NAME)
-OUTDIR=                 $(NAME)/out
-
-# Platform-independent settings
 USE_MACRO=              cpp
 WSPKG_IN_COMMON=        wscommon.h
-WSPKG_IN_PLATFORM_PKG=  $(INDIR)/$(NAME).pkg
-WSPKG_IN_PLATFORM_SED=  $(INDIR)/$(NAME).sed
-WSPKG_OUT_LIST=         $(OUTDIR)/$(NAME).list
-
-# Backend-specific settings
 BACKEND=                freebsd
-FREEBSD_OUT_PORTS=      $(OUTDIR)/$(NAME).ports
-FREEBSD_IN_MAKEFILE_IN= $(INDIR)/$(NAME).makefile.in
-FREEBSD_OUT_MAKEFILE=   $(OUTDIR)/$(NAME).makefile
+PKGNAME=                217
 
-FREEBSD_IN_PKGDESC=     $(INDIR)/$(NAME).local.217.pkg-descr
-FREEBSD_OUT_PKGDIR=     $(OUTDIR)/217
-FREEBSD_OUT_PKGDESC=    $(FREEBSD_OUT_PKGDIR)/pkg-descr
-FREEBSD_OUT_PKG=        $(FREEBSD_OUT_PKGDIR)/Makefile
-
-# Update the ports tree
-FREEBSD_OUT_PORTS_CATEGORY_MAKEFILE=     $(INDIR)/$(NAME).local.Makefile
-FREEBSD_OUT_PORTS_CATEGORY_MAKEFILE_INC= $(INDIR)/$(NAME).local.Makefile.inc
-FREEBSD_OUT_PORTS_CATEGORY=              local
-
+include $(WSPKGDIR)/wspkg-mk/defaults.mk
 include $(WSPKGDIR)/wspkg-mk/packages.mk
-
-clean:
-	rm -rf $(OUTDIR)
-
