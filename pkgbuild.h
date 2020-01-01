cd3ac0f41693007fd61dbb2e98d1cde3df75a2d8
Ting-Wei Lan <lantw44@gmail.com>
2020-01-02 01:17:59 +0800

    pkgbuild: Upgrade the default Python version to 3.7
    https://svnweb.freebsd.org/changeset/ports/520219

--- pkgbuild.h
+++ pkgbuild.h
@@ -1,6 +1,6 @@
 // Basic packages
  bash bash-completion bind-tools cmdwatch git htop ldapvi
- lighttpd lsof ncdu portlint pstree py36-ranger rsync screen
+ lighttpd lsof ncdu portlint pstree py37-ranger rsync screen
  subversion sudo texinfo tig tree vim w3m wget
 
 // Poudriere
