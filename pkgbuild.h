724151d0fafd45d67106b12d3599e7b204a09c1e
Ting-Wei Lan <lantw44@gmail.com>
2021-10-16 15:06:11 +0800

    wsbsd, pkgbuild: Adapt to git package split
    https://cgit.freebsd.org/ports/commit/?id=ff5ded75bcda28c5

--- pkgbuild.h
+++ pkgbuild.h
@@ -1,5 +1,5 @@
 // Basic packages
- bash bash-completion bind-tools cmdwatch git groff htop ldapvi
+ bash bash-completion bind-tools cmdwatch git git-svn groff htop ldapvi
  lighttpd lsof ncdu portlint pstree py38-ranger rsync screen
  subversion sudo texinfo tig tree vim@console w3m wget
 
