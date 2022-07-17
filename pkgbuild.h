8ec234418615c0f1883d4d5a50452ad7db53860f
Ting-Wei Lan <lantw44@gmail.com>
2022-07-17 10:14:51 +0800

    wsbsd, pkgbuild: Upgrade the default Python version to 3.9
    https://cgit.freebsd.org/ports/commit/?id=f117f2c485527927

--- pkgbuild.h
+++ pkgbuild.h
@@ -1,6 +1,6 @@
 // Basic packages
  bash bash-completion bind-tools cmdwatch git git-svn groff htop ldapvi
- lighttpd lsof ncdu portlint pstree py38-ranger rsync screen
+ lighttpd lsof ncdu portlint pstree py39-ranger rsync screen
  subversion sudo texinfo tig tree vim@console w3m wget
 
 // Poudriere
